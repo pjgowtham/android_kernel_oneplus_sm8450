05a51efa363f94334eaaf1fb3e2b8d06cac74372
pjgowtham <pjgowtham@gmail.com>
2023-12-28 14:22:55 +0530

    treewide: Address a few more clang-17 build warnings
    Change-Id: Ia086a4d6d6b993a49ec5c32cb368937c5930c74c

--- drivers/net/wireless/cnss2/qmi.c
+++ drivers/net/wireless/cnss2/qmi.c
@@ -650,7 +650,7 @@ static char *cnss_bdf_type_to_str(enum cnss_bdf_type bdf_type)
 
 #ifdef OPLUS_FEATURE_WIFI_BDF
 //Modify for: multi projects using different bdf
-static bool is_prj_support_region_id() {
+static bool is_prj_support_region_id(void) {
         int project_id = get_project();
         cnss_pr_dbg("project: %d\n", project_id);
         if (project_id == 20846 || project_id == 20847 || project_id == 133194
@@ -660,7 +660,7 @@ static bool is_prj_support_region_id() {
         return false;
 }
 
-static bool is_udon_internal_prj_need_second_rescouse() {
+static bool is_udon_internal_prj_need_second_rescouse(void) {
         int project_id = get_project();
         cnss_pr_dbg("project: %d\n", project_id);
         if (project_id == 22803) {
@@ -669,7 +669,7 @@ static bool is_udon_internal_prj_need_second_rescouse() {
         return false;
 }
 
-static bool is_udon_export_prj_need_second_rescouse() {
+static bool is_udon_export_prj_need_second_rescouse(void) {
         int project_id = get_project();
         cnss_pr_dbg("project: %d\n", project_id);
         if (project_id == 22881) {
