914935583f0ce199114a4a1f31178ab25a3a052d
pjgowtham <pjgowtham@gmail.com>
2026-02-01 20:42:34 +0530

    oplus_touchscreen_v2: Remove MIN/MAX definitions that already exist in headers

--- drivers/input/touchscreen/oplus_touchscreen_v2/Synaptics/Syna_tcm_S3910/synaptics_tcm_S3910.h
+++ drivers/input/touchscreen/oplus_touchscreen_v2/Synaptics/Syna_tcm_S3910/synaptics_tcm_S3910.h
@@ -85,16 +85,6 @@
 		} \
 	} while (0)
 
-#define MAX(a, b) \
-	({__typeof__(a) _a = (a); \
-	__typeof__(b) _b = (b); \
-	_a > _b ? _a : _b; })
-
-#define MIN(a, b) \
-	({__typeof__(a) _a = (a); \
-	__typeof__(b) _b = (b); \
-	_a < _b ? _a : _b; })
-
 #define STR(x) #x
 
 #define CONCAT(a, b) a##b
